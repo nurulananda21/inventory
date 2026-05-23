147b977221e4270b62ef6e70854f0ff0f451a6ab
nurulananda21 <nurulananda21>
2026-05-23 07:43:52 -0700

    update routes

--- inventory/routes/web.php
+++ inventory/routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/items', function () {
-    return response()->json([
-        'status' => 'success'
-    ]);
+Route::get('/', function () {
+    return 'Laravel berjalan';
 });
